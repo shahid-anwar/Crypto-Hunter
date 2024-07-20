bdbe542eaedaf1673832cac02986c8719455ab9d
shahid-anwar <shahidanwar3548@gmail.com>
2024-07-20 20:16:59 +0530

    Project Initialised

--- crypto-hunter/src/App.jsx
+++ crypto-hunter/src/App.jsx
@@ -1,23 +1,7 @@
 // import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Header from "./Component/Header";
-import AllRoutes from "./Routes/AllRoutes";
-import { makeStyles } from "@material-ui/core/styles";
 function App() {
-  const useStyles = makeStyles({
-    App: {
-      backgroundColor: "#14161a",
-      color: "white",
-      minHeight: "100vh",
-    },
-  });
-  const classes = useStyles();
-  return (
-    <div className={classes.App}>
-      <Header />
-      <AllRoutes />
-    </div>
-  );
+  return <div className={App}></div>;
 }
 
 export default App;
--- crypto-hunter/src/main.jsx
+++ crypto-hunter/src/main.jsx
@@ -1,13 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import CryptoContext from "./Context/CryptoContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <CryptoContext>
-      <App />
-    </CryptoContext>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
